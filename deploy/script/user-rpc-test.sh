8b865ed94aaa63a73cfbd8f0acdb3b4788a28a8b
xyl <416319808@qq.com>
2024-11-04 23:18:02 +0800

    指定注册etcd的ip

--- deploy/script/user-rpc-test.sh
+++ deploy/script/user-rpc-test.sh
@@ -3,6 +3,7 @@ reso_addr='registry.cn-hangzhou.aliyuncs.com/0rz/user-rpc-dev'
 tag='latest'
 
 container_name="im-user-rpc-test"
+pod_ip="192.168.2.26"
 
 # 停止容器
 docker stop ${container_name}
@@ -19,4 +20,4 @@ docker pull ${reso_addr}:${tag}
 
 # 如果需要指定配置文件的
 # docker run -p 10001:8080 --network imooc_easy-chat -v /easy-chat/config/user-rpc:/user/conf/ --name=${container_name} -d ${reso_addr}:${tag}
-docker run -p 10000:10000  --name=${container_name} -d ${reso_addr}:${tag}
+docker run -p 10000:10000 -e POD_IP=${pod_ip} --name=${container_name} -d ${reso_addr}:${tag}
